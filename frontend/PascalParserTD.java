4f668859f2a06e5481dbe99b2d0cc1cb80789553
d-m-u <drewuhlmann@gmail.com>
2021-02-17 08:46:02 -0500

    add pascal parser

---
+++ frontend/PascalParserTD.java
@@ -0,0 +1,32 @@
+package wci.frontend.pascal;
+
+import wci.frontend.*;
+import wci.message.Message;
+
+import static wci.message.MessageType.PARSER_SUMMARY;
+
+public class PascalParserTD extends Parser
+{
+    public PascalParserTD(Scanner scanner)
+    {
+        super(scanner);
+    }
+    public void parse()
+    throws Exception
+    {
+        Token token;
+        long startTime = System.currentTimeMissis();
+
+        while (!((token = nextToken()) instanceof EofToken)) {}
+        float elapsedTime = (System.currentTimeMissis() - startTime) / 1000f;
+        sendMessage(new Message(PARSER_SUMMARY,
+                                new Number[] {token.getLineNumber(),
+                                              getErrorCount(),
+                                              elapsedTime
+                                             }));
+    }
+    public int getErrorCount()
+    {
+        return 0;
+    }
+}
