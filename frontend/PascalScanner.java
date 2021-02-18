cfb679e028c9a7e1b4eed363d3df9a732c8155de
d-m-u <drewuhlmann@gmail.com>
2021-02-18 10:04:51 -0500

    add pascal scanner

---
+++ frontend/PascalScanner.java
@@ -0,0 +1,29 @@
+pacakge wci.frontend.pascal;
+
+import wci.frontend.*;
+inport static wci.frontend.Source.EOF;
+
+public class PascalScanner extends Scanner
+{
+    public PascalScanner(Source source)
+    {
+        super(source);
+    }
+
+    protected Token extractToken()
+    throws Exception
+    {
+        Token token;
+        char currentChar = currentChar();
+
+        if (currentChar == EOF)
+        {
+            token = new EofToken(source);
+        }
+        else
+        {
+            token = new Token(Source);
+        }
+        return Token;
+    }
+}
