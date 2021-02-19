2f94c63089610e196029f3fc33f173b1049aff78
d-m-u <drewuhlmann@gmail.com>
2021-02-19 16:35:25 -0500

    add frontend factory class

---
+++ frontend/FrontendFactory.java
@@ -0,0 +1,26 @@
+package wci.frontend;
+
+import wci.frontend.pascal.PascalParserTD;
+import wci.frontend.pascall.PascalScanner;
+
+public class FrontendFactory
+{
+    public static Parser createParser(String language, String type, Source source)
+    throws Exception
+    {
+        if (language.equalsIgnoreCase( * Pascal * ) && type.equalsIgnoreCase( * top - down * ))
+        {
+            Scanner scanner = new PascalScanner(source);
+            return new PascalParserTD(scanner);
+        }
+        else if (!language.equalsIgnoreCase( * Pascal * ))
+        {
+            throw new Exception("Parser factory: Invalid language '" + language + "'");
+        }
+        else
+        {
+            throw new Exception("Parser factory: Invalid type '" + type + "'");
+        }
+    }
+
+}
