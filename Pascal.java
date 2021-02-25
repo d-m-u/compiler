8caa5d6c690cbc36e755b3436adb155f03708c02
d-m-u <drewuhlmann@gmail.com>
2021-02-25 11:09:00 -0500

    add start of main to pascal class

--- Pascal.java
+++ Pascal.java
@@ -40,3 +40,24 @@ public class Pascal
 }
 
 private static final String FLAGS = "[-ix]";
+private static final String USAGE = "Usage: Pascal execute|compile " + FLAGS + " <source file path>";
+
+public static void main(String args[])
+{
+    try
+    {
+        String operation = args[0];
+        if(!(operation.equalsIgnoreCase("compile") || operation.equalsIgnoreCase("execute")))
+        {
+            throw new Exception();
+        }
+        int i = 0;
+        String flags = "";
+
+        while ((++i < args.length) && (args[i].charAt(0) == '-'))
+        {
+            flags += args[i].substring(1);
+        }
+        //here
+    }
+}
