a97d01e4d0688a97a94205b04397b3cb4f303645
d-m-u <drewuhlmann@gmail.com>
2021-02-28 16:12:43 -0500

    add message received method

--- Pascal.java
+++ Pascal.java
@@ -137,5 +137,31 @@ private static final String COMPILER_SUMMARY_FORMAT =
 
 private class BackendMessageListener implements MessageListener
 {
-    //here
+    public void messageReceived(Message message)
+    {
+    	MessageType type = message.getType();
+
+    	switch(type){
+    		case INTERPRETER_SUMMARY: 
+
+    		{
+    			Number body[] = (Number[]) message.getBody();
+    			int executionCount = (Integer) body[0];
+    			int runtimeErrors = (Integer) body[1];
+    			float elapsedTime = (Float) body[2];
+
+    			System.out.printf(INTERPRETER_SUMMARY_FORMAT, executionCount, runtimeErrors, elapsedTime);
+    			break;
+    		}
+    		case COMPILER_SUMMARY: {
+    			Number body[] = (Number[]) message.getBody();
+    			int instructionCount = (Integer) body[0];
+    			float elapsedTime = (Float) body[1];
+
+    			System.out.printf(COMPILER_SUMMARY_FORMAT, instructionCount, elapsedTime)
+
+    			break;
+    		}
+    	}
+    }
 }
