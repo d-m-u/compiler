e52d2437033184d47ac3d4ba48939cf6706878f5
d-m-u <drewuhlmann@gmail.com>
2021-03-08 06:51:25 -0500

    add getters and setters to backend.java

--- wci/backend/Backend.java
+++ wci/backend/Backend.java
@@ -16,7 +16,35 @@ public abstract class Backend implements MessageProducer
     protected SymTab symTab;
     protected Icode iCode;
 
+     public ICode getICode()
+    {
+        return iCode;
+    }
+
+ 		public SymTab getSymTab()
+    {
+        return symTab;
+    }
+		public MessageHandler getMessageHandler()
+    {
+        return messageHandler;
+    }
+
     public abstract void process(ICode iCode, SymTab symTab)
     throws Exception;
 
+		public void sendMessage(Message message)
+    {
+        messageHandler.sendMessage(message);
+    }
+
+    public void addMessageListener(MessageListener listener)
+    {
+        messageHandler.addListener(listener);
+    }
+
+    public void removeMessageListener(MessageListener listener)
+    {
+        messageHandler.removeListener(listener);
+    }
 }
