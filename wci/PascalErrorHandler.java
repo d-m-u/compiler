572f8f57b72a36cce1fb021c6da825f8ae9741a0
d-m-u <drewuhlmann@gmail.com>
2021-03-23 15:14:39 -0400

    create error handler class skeleton

---
+++ wci/PascalErrorHandler.java
@@ -0,0 +1,14 @@
+package wci.frontend.pascal;
+
+import wci.frontend.*;
+import wci.message.Message; 
+
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
+import static wci.message.MessageType.SYNTAX_ERROR;
+
+public class PascalErrorHandler
+{
+	private static final int MAX_ERRORS = 25;
+	
+}
