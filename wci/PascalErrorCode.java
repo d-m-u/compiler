5bd84a3a2277a7fbf8ed459cb6309eee15af5a30
d-m-u <drewuhlmann@gmail.com>
2021-03-26 16:50:48 -0400

    add start of pascal error code list

---
+++ wci/PascalErrorCode.java
@@ -0,0 +1,9 @@
+package wci.frontend.pascal;
+
+public enum PascalErrorCode {
+	ALREADY_FORWARDED("Already specified in FORWARD"), 
+	IDENTIFIER_REDEFINED("Redefined identifier"),
+	IDENTIFIER_UNDEFINED("Undefined identifier"),
+	INCOMPATIBLE_ASSIGNMENT("Incompatible assignment"),
+(""),
+}
